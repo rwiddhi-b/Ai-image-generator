1c76b3d9a8d9b84d9c22b787a584fbdd859cbfa3
rwiddhi-b <bhowmikrwiddhi@gmail.com>
2023-07-20 10:12:56 +0530

    initial commit

---
+++ Client/src/Components/index.js
@@ -0,0 +1,9 @@
+import Cards from "./Cards";
+import Loader from "./loader";
+import FormField from "./FormField";
+
+export {
+    Cards,
+    FormField,
+    Loader
+}
---
+++ Server/mongodb/connect.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const connectDB = (url) => {
+    mongoose.set('strictQuery', true);
+
+    mongoose.connect(url)
+        .then(() => console.log('Connected to mongo'))
+        // .catch((err) => {
+        //     console.error('failed to connect with mongo');
+        //     console.error(err);
+        // });
+        .catch((err) => console.error(err)
+        );
+};
+
+export default connectDB;
